254ff6d935a6ed0be8962157b7ae726ee32d17eb
Ian Brenner <ianbrenner6@gmail.com>
2018-05-22 15:58:52 -0400

    Added comments to show what the code's doing.

--- KeyGen.py
+++ KeyGen.py
@@ -1,24 +1,31 @@
 #! python 3
 
+# KeyGen.py -> A simple CD Key Generator, written in python.
+
 import random, sys
 
 class KeyGen():
-	
+
 	def __init__(self):
 		global i
 		i = int(input("How many serial codes are you looking for? \n"))
 		print("")
 		self.main(i)
+# This allows the user to input how many iterations of a key generation they'd like.
 
 	def main(self, count):
-		""" """
+		""" Our main iteration function, using simple 
+		capital letters, along with the numbers 0-9 """
 		seq = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-		store = []
 
 		for i in range(count):
 			print('-'.join(''.join(random.choice(seq) for _ in range(5)) for _ in range(5)))
+# Our loop used to iterate through the range of our count parameter, and print our keys to the console
+## in sequences of 5 letters/numbers.
 
 		print("\nCreated {} serial keys for you, m'lord.".format(count))
+
+# And there's our print statement.
 
 if __name__ == '__main__':
 	app = KeyGen()
